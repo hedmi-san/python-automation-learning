b7f658b35174926c15a7ac602015509adfcabab3
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-08-02 22:09:09 +0100

    manipulating strings

--- manipulatingstring.py
+++ manipulatingstring.py
@@ -2,3 +2,12 @@ spam = ','.join(['cats','rats','dogs','horses'])
 print(spam + 'together')
 
 print(spam.split())
+a ='MyABCnameABCisABCSimon'.split('ABC')
+print(a)
+
+b = 'My name is Simon'.split('m')
+print(b)
+before, sep,after = 'Hello, world!'.partition(' ')
+print(before)
+print(sep)
+print(after)
